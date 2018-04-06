943413a0d912ce53f45639d3e2ab79949453a9a0
eygitadmin <bramaswamy@etihad.ae>
2018-04-06 18:27:47 +0400

    testing...
    updated index.html, server.js for transaction submission and retrieval between local and Rinkeby seamlessly.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const abiDecoder = require('abi-decoder');
 //const solc = require('solc');
 //TODO: externalize to properties file
 
-var PORT = process.env.PORT || 5050 ;  //for bluemix
+var PORT = process.env.PORT || 5050;  //for bluemix
 
 var web3 = new Web3();
 var app = express();
@@ -22,31 +22,59 @@ app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static('public')); //public folder for static content
 
+var useProvider = "local"; //set 'local' to use Ganache on local mache or 'remote' to use rinkeby or other provider
+
 //Defaults 7545 for Ganache , 8545 for testrpc/Ganache-cli
 //TODO: externalize to properties file
-var providerLocation = 'http://localhost:7545';
+var providerLocation = '';
+var ganacheLocation = 'http://localhost:7545';
 var rinkebyLocation = "https://rinkeby.infura.io/9PBP4aZ5wsWnylDXZIUp";
 var rinkebyRPCLocation = "https://rinkeby.infura.io/9PBP4aZ5wsWnylDXZIUp:8545";
 
-
-web3.setProvider(new Web3.providers.HttpProvider(rinkebyLocation));
-/**
- * 
- * contract addresses:
- * 0x8cdaf0cd259887258bc13a92c0a6da92698644c0
- * 
- */
-var contractAddress = "0xe2dB8D5ED69D94fe01183DC3607aA80dC3e190C5";
 var forAccount = "0x469f17e6534ad8d765403f46bf86740b4fb668dc";
-             
+var contractAddress;
 
-//var input = fs.readFileSync('./contracts/ContractLG.sol').toString();
-//console.log(input);
-//var compiledContract = solc.compile(input);
-//var input = {'ContractLG.sol':fs.readFileSync('./contracts/ContractLG.sol', 'utf8')}
-//let compiledContract = solc.compile({sources : input}, 1);
-//console.log(compiledContract);
-//let abi = JSON.parse(compiledContract.contracts[':ContractLG'].interface);
+if (useProvider == 'local') {
+	providerLocation = ganacheLocation;
+	forAccount = "0x627306090abaB3A6e1400e9345bC60c78a8BEf57";
+} else {
+	providerLocation = rinkebyLocation;
+	forAccount = "0x469f17e6534ad8d765403f46bf86740b4fb668dc";
+}
+
+
+//(Re)Set based on instance used
+web3.setProvider(new Web3.providers.HttpProvider(providerLocation));
+
+function prepareContractsList() {
+	var contList;
+	console.log("getting contracts list")
+	if (useProvider == "local") {
+		console.log("Using contracts on local...");
+		contList = [
+			{ "address": "0", "name": "Please select a contract" },
+			{ "address": "0x345ca3e014aaf5dca488057592ee47305d9b3e10", "name": "A320 Contract - EYCON 54589" },
+			{ "address": "0xf25186b5081ff5ce73482ad761db0eb0d25abfbf", "name": "A380 Contract - EYCON 67683" }
+		];
+
+		forAccount = "";
+	} else {
+		contList = [
+			{ "address": "0", "name": "Please select a contract" },
+			{ "address": "0xe2dB8D5ED69D94fe01183DC3607aA80dC3e190C5", "name": "B777 Contract - EYCON 54589" },
+			{ "address": "0x5066667073799d733e3513c0c8cecf4cc3a774bf", "name": "B787 Contract - EYCON 67683" }
+		];
+
+		forAccount = "0x469f17e6534ad8d765403f46bf86740b4fb668dc";
+	}
+
+	return contList;
+}
+
+
+app.post('/loadcontracts', function (req, res) {
+	res.json(prepareContractsList());
+});
 
 var contractABI = [
 	{
@@ -304,114 +332,224 @@ app.get('/api/contractinst', function (req, res) {
 });
 
 
+function getTransactionsByAccount(myaccount, contradd, startBlockNumber, endBlockNumber) {
+	if (endBlockNumber == null) {
+		new web3.eth.getBlockNumber(function (err, res) {
+			if (err) {
+				console.log("Error while getting block number...");
+			} else {
+				endBlockNumber = res;
+				console.log("Using endBlockNumber: " + endBlockNumber);
+
+				if (startBlockNumber == null) {
+					startBlockNumber = 0;
+					//startBlockNumber = endBlockNumber - 1000;
+					console.log("Using startBlockNumber: " + startBlockNumber);
+				}
+				console.log("Searching for transactions to/from account \"" + myaccount + "\" within blocks " + startBlockNumber + " and " + endBlockNumber);
+
+				for (var i = startBlockNumber; i <= endBlockNumber; i++) {
+					/*if (i % 1000 == 0) {
+						console.log("Searching block " + i);
+					}*/
+					var block = new web3.eth.getBlock(i, true, function (err, resp) {
+						if (err) {
+							console.log("Error while getting a block...");
+						} else {
+							console.log("Printing block: " + resp);
+							if (resp != null && resp.transactions != null) {
+								resp.transactions.forEach(function (e) {
+									if (myaccount == e.from || contradd == e.to) {
+
+										console.log("  tx hash          : " + e.hash + "\n"
+											+ "   nonce           : " + e.nonce + "\n"
+											+ "   blockHash       : " + e.blockHash + "\n"
+											+ "   blockNumber     : " + e.blockNumber + "\n"
+											+ "   transactionIndex: " + e.transactionIndex + "\n"
+											+ "   from            : " + e.from + "\n"
+											+ "   to              : " + e.to + "\n"
+											+ "   value           : " + e.value + "\n"
+											+ "   time            : " + block.timestamp + " " + new Date(block.timestamp * 1000).toGMTString() + "\n"
+											+ "   gasPrice        : " + e.gasPrice + "\n"
+											+ "   gas             : " + e.gas + "\n"
+											+ "   input           : " + e.input);
+
+										console.log("------------------*******------------------");
+										var gotinput = abiDecoder.decodeMethod(e.input);
+										//var gotinput = getTxDecodedInput(e.hash);
+										console.log(gotinput);
+										console.log("------------------*******------------------");
+									}
+								})
+							}
+						}
+					});
+				}
+			}
+		});
+	}
+}
+
 // api to get txns at an address at rinkeby 
 // http://localhost:5050/api/txn/0x469f17e6534ad8d765403f46bf86740b4fb668dc
 app.get('/api/txn/:address', function (req, res) {
 	var address = req.params.address;
-	var api_key = "C2DBPWS7DTZDGEPBC34MWV8D9SCT31PE5E";
-	console.log('>> Entered /api/txn/' + address);
-	var invokeurl = "http://api-rinkeby.etherscan.io/api?module=account&action=txlist&address=" + address + "&startblock=0&endblock=99999999&sort=asc&apikey=" + api_key;
-	request.get(invokeurl, function (error, response, body) {
-		if (error) {
-			return console.log(error);
-		}
-		var bodyObj = JSON.parse(body);
-		var results = bodyObj.result;
 
-		var encrInp;
-
-		// decode the transaction input
-		for (var item in results) {
-			encrInp = results[item].input;
-			results[item].input = abiDecoder.decodeMethod(encrInp);
-
-			// delete unwanted object attributes in response
-			delete results[item].from;
-			delete results[item].to;
-			delete results[item].blockHash;
-			delete results[item].gas;
-			delete results[item].gasPrice;
-			delete results[item].transactionIndex;
-			delete results[item].isError;
-			delete results[item].txreceipt_status;
-			delete results[item].nonce;
-			delete results[item].value;
-			delete results[item].cumulativeGasUsed;
-			delete results[item].gasUsed;
-			delete results[item].confirmations;
-		}
-		res.json(results);
-		console.log('<< Responded /api/txn/' + address + " with " + results.length + " transactions");
-
-	});
-});
-
-
-app.post('/recordcontract', function (req, res) {            
-	// TO BE DONE
-	// 1. index.html to sent the address of the Contract Instance against which txns are made
-	// 2. to get freaking sign the transaction.
-	
-	var account = web3.eth.accounts.privateKeyToAccount('0x678f26f38fad4f434bffd435435c3416afd627e280fad373e3e3ee8d78870a99');
-	web3.eth.accounts.wallet.add(account);  //adding account to wallet
-
-	if (req.method == 'POST') {
-		// console.log("ABI: " + contractABI);
-		console.log("ContractAddress: " + contractAddress + " Account address :" + account.address);
-		var theContract = new web3.eth.Contract(contractABI, contractAddress);
-
-		theContract.options.from = account.address ; // default from address
-		theContract.options.gasPrice = '2'; // default gas price in wei
-		theContract.options.gas = 5000000; // provide as fallback always 5M gas
-
-		console.log("Inside POST.." + req.body.title);
-		theContract.methods.setTitle(req.body.title).send( function (err, res) {
-		//theContract.methods.setTitle(req.body.title).send({ from: account.address }, function (err, res) {
-			
-			if (err) {
-				console.log('oh no...' + err.message);
-			} else {
-				console.log('hurray...' + res);
+	if (useProvider == "local") {
+		console.log('Fetching transactions for contract ' + contractAddress);
+		getTransactionsByAccount(forAccount, contractAddress);
+		//send the response back to the html code...
+	} else {
+		var api_key = "C2DBPWS7DTZDGEPBC34MWV8D9SCT31PE5E";
+		console.log('>> Entered /api/txn/' + address);
+		var invokeurl = "http://api-rinkeby.etherscan.io/api?module=account&action=txlist&address=" + address + "&startblock=0&endblock=99999999&sort=asc&apikey=" + api_key;
+		request.get(invokeurl, function (error, response, body) {
+			if (error) {
+				return console.log(error);
 			}
+			var bodyObj = JSON.parse(body);
+			var results = bodyObj.result;
+
+			var encrInp;
+
+			// decode the transaction input
+			for (var item in results) {
+				encrInp = results[item].input;
+				results[item].input = abiDecoder.decodeMethod(encrInp);
+
+				// delete unwanted object attributes in response
+				delete results[item].from;
+				delete results[item].to;
+				delete results[item].blockHash;
+				delete results[item].gas;
+				delete results[item].gasPrice;
+				delete results[item].transactionIndex;
+				delete results[item].isError;
+				delete results[item].txreceipt_status;
+				delete results[item].nonce;
+				delete results[item].value;
+				delete results[item].cumulativeGasUsed;
+				delete results[item].gasUsed;
+				delete results[item].confirmations;
+			}
+			res.json(results);
+			console.log('<< Responded /api/txn/' + address + " with " + results.length + " transactions");
 		});
 	}
 });
 
-/*
-app.post('/getTransactions', function (req, res) {
-	console.log('Fetching transactions for contract ' + contractAddress);
-	// Fetch all transaction logs with the specified address
-    var filter = web3.eth.Filter({fromBlock: 0, toBlock: 'latest', address: contractAddress}, function(error, result) {
-		// Get all entries
-		var results = filter.get(function(error, result){
-			if (!error)
-			  console.log("[I] Fetched all transactions sent or sent to " + contractAddress);
-			 else
-			  console.log("[E] An error has occurred " + error);
-		});
-	
-		var json_tuple;
-		// Iterate through the transactions in the logs 
-		for(var log in results) {
-			var log_tx_hash = log.transactionHash;
-			console.log("Transaction has : " + log_tx_hash);
-			// Lookup transaction with hash
-			var tx = web3.eth.getTransaction(log_tx_hash);
-			// Check the to and from addresses. We skip transactions unrelated to the current sender
-			if(tx.from === "0x627306090abab3a6e1400e9345bc60c78a8bef57") {
-				// Parse transaction data and check recipient
-				json_tuple = JSON.parse(tx.input);
-				if(field in json_tuple) {
-					//Do something with a field of the input data sent
-					// as a JSON object
-					console.log("We have found a transaction with data: " + json_tuple[[field]]);
-					console.log("----------------****-----------------");
+
+app.post('/recordcontract', function (req, res) {
+	// TO BE DONE
+	// 1. index.html to sent the address of the Contract Instance against which txns are made
+	// 2. to get freaking sign the transaction.
+	if (useProvider == 'local') {
+		contractAddress = req.body.contaddr;
+		forAccount = '0x627306090abaB3A6e1400e9345bC60c78a8BEf57';
+		if (req.method == 'POST') {
+			// console.log("ABI: " + contractABI);
+			console.log("ContractAddress: " + contractAddress + " Account address :" + forAccount);
+			var theContract = new web3.eth.Contract(contractABI, contractAddress);
+
+			console.log("Inside POST.." + req.body.title);
+			//theContract.methods.setTitle(req.body.title).send( function (err, res) {
+			theContract.methods.setTitle(req.body.title).send({ from: forAccount }, function (err, res) {
+				if (err) {
+					console.log('Title: oh no...' + err.message);
+				} else {
+					console.log('Title: hurray...' + res);
 				}
-			}
+			});
+
+			theContract.methods.setStatus(req.body.status).send({ from: forAccount }, function (err, res) {
+				if (err) {
+					console.log('Status: oh no...' + err.message);
+				} else {
+					console.log('Status: hurray...' + res);
+				}
+			});
+
+			theContract.methods.setValidity(req.body.commdate, req.body.tenure, req.body.wardetails).send({ from: forAccount }, function (err, res) {
+				if (err) {
+					console.log('Validity: oh no...' + err.message);
+				} else {
+					console.log('Validity: hurray...' + res);
+				}
+			});
+			theContract.methods.setAddress(req.body.eyaddress, req.body.contraddress).send({ from: forAccount }, function (err, res) {
+				if (err) {
+					console.log('Address: oh no...' + err.message);
+				} else {
+					console.log('Address: hurray...' + res);
+				}
+			});
+			theContract.methods.setDescription(req.body.contrscope).send({ from: forAccount }, function (err, res) {
+				if (err) {
+					console.log('Description: oh no...' + err.message);
+				} else {
+					console.log('Description: hurray...' + res);
+				}
+			});
 		}
-	});
+	} else {
+		//Rinkeby private key for account 0x469f17e6534ad8d765403f46bf86740b4fb668dc
+		//0x678f26f38fad4f434bffd435435c3416afd627e280fad373e3e3ee8d78870a99
+		var privateKey = '0x678f26f38fad4f434bffd435435c3416afd627e280fad373e3e3ee8d78870a99';
+
+		var account = web3.eth.accounts.privateKeyToAccount(privateKey);
+		web3.eth.accounts.wallet.add(account);  //adding account to wallet
+
+		if (req.method == 'POST') {
+			// console.log("ABI: " + contractABI);
+			console.log("ContractAddress: " + contractAddress + " Account address :" + account.address);
+			var theContract = new web3.eth.Contract(contractABI, contractAddress);
+
+			theContract.options.from = account.address; // default from address
+			theContract.options.gasPrice = '2'; // default gas price in wei
+			theContract.options.gas = 5000000; // provide as fallback always 5M gas
+
+			console.log("Inside POST.." + req.body.title);
+			theContract.methods.setTitle(req.body.title).send( function (err, res) {
+			//theContract.methods.setTitle(req.body.title).send({ from: account.address }, function (err, res) {
+				if (err) {
+					console.log('Title: oh no...' + err.message);
+				} else {
+					console.log('Title: hurray...' + res);
+				}
+			});
+
+			theContract.methods.setStatus(req.body.status).send( function (err, res) {
+				if (err) {
+					console.log('Status: oh no...' + err.message);
+				} else {
+					console.log('Status: hurray...' + res);
+				}
+			});
+
+			theContract.methods.setValidity(req.body.commdate, req.body.tenure, req.body.wardetails).send( function (err, res) {
+				if (err) {
+					console.log('Validity: oh no...' + err.message);
+				} else {
+					console.log('Validity: hurray...' + res);
+				}
+			});
+			theContract.methods.setAddress(req.body.eyaddress, req.body.contraddress).send( function (err, res) {
+				if (err) {
+					console.log('Address: oh no...' + err.message);
+				} else {
+					console.log('Address: hurray...' + res);
+				}
+			});
+			theContract.methods.setDescription(req.body.contrscope).send( function (err, res) {
+				if (err) {
+					console.log('Description: oh no...' + err.message);
+				} else {
+					console.log('Description: hurray...' + res);
+				}
+			});
+		}
+	}
 });
-*/
 
 app.post('/getcontract', function (req, res) {
 	console.log("Getting contract details...");
@@ -494,58 +632,3 @@ function getTxDecodedInput(txhash) {
 
 }
 
-function getTransactionsByAccount(myaccount, startBlockNumber, endBlockNumber) {
-	if (endBlockNumber == null) {
-		new web3.eth.getBlockNumber(function (err, res) {
-			if (err) {
-				console.log("Error while getting block number...");
-			} else {
-				endBlockNumber = res;
-				console.log("Using endBlockNumber: " + endBlockNumber);
-
-				if (startBlockNumber == null) {
-					startBlockNumber = 0;
-					//startBlockNumber = endBlockNumber - 1000;
-					console.log("Using startBlockNumber: " + startBlockNumber);
-				}
-				console.log("Searching for transactions to/from account \"" + myaccount + "\" within blocks " + startBlockNumber + " and " + endBlockNumber);
-
-				for (var i = startBlockNumber; i <= endBlockNumber; i++) {
-					/*if (i % 1000 == 0) {
-						console.log("Searching block " + i);
-					}*/
-					var block = new web3.eth.getBlock(i, true, function (err, resp) {
-						if (err) {
-							console.log("Error while getting a block...");
-						} else {
-							console.log("Printing block: " + resp);
-							if (resp != null && resp.transactions != null) {
-								resp.transactions.forEach(function (e) {
-									//if (myaccount == "*" || myaccount == e.from || myaccount == e.to) {
-									console.log("  tx hash          : " + e.hash + "\n"
-										+ "   nonce           : " + e.nonce + "\n"
-										+ "   blockHash       : " + e.blockHash + "\n"
-										+ "   blockNumber     : " + e.blockNumber + "\n"
-										+ "   transactionIndex: " + e.transactionIndex + "\n"
-										+ "   from            : " + e.from + "\n"
-										+ "   to              : " + e.to + "\n"
-										+ "   value           : " + e.value + "\n"
-										+ "   time            : " + block.timestamp + " " + new Date(block.timestamp * 1000).toGMTString() + "\n"
-										+ "   gasPrice        : " + e.gasPrice + "\n"
-										+ "   gas             : " + e.gas + "\n"
-										+ "   input           : " + e.input);
-
-									console.log("------------------*******------------------");
-									console.log("Contract: " + e.to);
-									console.log(web3.utils.toAscii(e.input));
-									console.log("------------------*******------------------");
-									//}
-								})
-							}
-						}
-					});
-				}
-			}
-		});
-	}
-}
